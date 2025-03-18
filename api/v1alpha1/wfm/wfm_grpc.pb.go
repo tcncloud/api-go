b1b449df8c320814aa9527fbeef6186531c939d8
TCN CI <ci@tcnp3.com>
2025-03-18 20:32:23 +0000

    update api-go > Merge branch '883-add-schedule-type-field-to-shift-lister' into 'master'
    Resolve "Add schedule type field to shift listers"
    
    Closes #883
    
    See merge request tcnpublic/api!1032

--- api/v1alpha1/wfm/wfm_grpc.pb.go
+++ api/v1alpha1/wfm/wfm_grpc.pb.go
@@ -1362,6 +1362,7 @@ type WFMClient interface {
 	// @include_shift_segments must be true to take effect.
 	// If @include_activity is set to true then the related non skill activity for the scheduling activity will be returned in the scheduling
 	// activities member non skill activity field. @include_scheduling_activity must be true to take effect.
+	// The given @shift_instance_sids must all belong to the given @schedule_type.
 	// Errors:
 	//   - grpc.Invalid: @shift_instance_sids in the request are invalid.
 	//   - grpc.Internal: error occurs when listing the shift instances or their shift segments.
@@ -1485,6 +1486,7 @@ type WFMClient interface {
 	// Lists shift segments for the specified shift instances for the org sending the request.
 	// If @include_scheduling_activity is set to true then the related scheduling activity for the shift segment will be returned in the scheduling activity field.
 	// If @include_activity is set to true then the related non skill activity for the scheduling activity will be returned in the scheduling activities member non skill activity field.
+	// The given @shift_instance_sids must all belong to the given @schedule_type.
 	// Errors:
 	//   - grpc.Invalid: the request data is invalid.
 	//   - grpc.NotFound: a shift instance doesn't exist.
@@ -5748,6 +5750,7 @@ type WFMServer interface {
 	// @include_shift_segments must be true to take effect.
 	// If @include_activity is set to true then the related non skill activity for the scheduling activity will be returned in the scheduling
 	// activities member non skill activity field. @include_scheduling_activity must be true to take effect.
+	// The given @shift_instance_sids must all belong to the given @schedule_type.
 	// Errors:
 	//   - grpc.Invalid: @shift_instance_sids in the request are invalid.
 	//   - grpc.Internal: error occurs when listing the shift instances or their shift segments.
@@ -5871,6 +5874,7 @@ type WFMServer interface {
 	// Lists shift segments for the specified shift instances for the org sending the request.
 	// If @include_scheduling_activity is set to true then the related scheduling activity for the shift segment will be returned in the scheduling activity field.
 	// If @include_activity is set to true then the related non skill activity for the scheduling activity will be returned in the scheduling activities member non skill activity field.
+	// The given @shift_instance_sids must all belong to the given @schedule_type.
 	// Errors:
 	//   - grpc.Invalid: the request data is invalid.
 	//   - grpc.NotFound: a shift instance doesn't exist.
